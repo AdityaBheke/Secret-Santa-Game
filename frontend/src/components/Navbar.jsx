e4f892203accf079dbfab18445a95ff8c413668f
Aditya Bheke <adityabheke34@gmail.com>
2025-02-15 14:27:57 +0530

    create basic structure for frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="nav-item brand-name">Secret Santa</div>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+    return <div className="main">
+        <div className="home-header">&quot;A Little Mystery, A Lot of Joy!&quot;</div>
+    </div>
+}
